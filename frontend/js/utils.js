5b309462f53ad39cd039f4ebdb0afc5c312d3c3d
Katherine Varela <katherinevarela775@gmail.com>
2026-03-17 23:50:38 -0300

    feat(utils): agregar funciones getToken, setToken, removeToken (Katherine Varela)
    - getToken(): lee token del localStorage
    - setToken(token): guarda token en localStorage
    - removeToken(): limpia token y user del localStorage
    
    Co-Authored-By: Katherine Varela <noreply@github.com>

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,3 +1,16 @@
 // utils.js - Utilidades generales de la aplicacion
 
 const API_BASE = 'http://127.0.0.1:5000';
+
+function getToken() {
+    return localStorage.getItem('token');
+}
+
+function setToken(token) {
+    localStorage.setItem('token', token);
+}
+
+function removeToken() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+}
